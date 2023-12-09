d6b85391f69b0f383b882eeb6601c22814ce4ae5
NgocLe152002 <2124801030164@student.tdmu.edu.vn>
2023-12-09 14:05:07 +0700

    abcd123
    adf

--- STV/Controllers/StoryController.cs
+++ STV/Controllers/StoryController.cs
@@ -13,7 +13,7 @@ namespace STV.Controllers
     public class StoryController : Controller
     {
         // GET: Story
-        dbSTVDataContext db = new dbSTVDataContext("Data Source=LAPTOP-4PHTMN7E;Initial Catalog=Nhom6;Integrated Security=True");
+        dbSTVDataContext db = new dbSTVDataContext("Data Source=LAPTOP010502\\SQLEXPRESS;Initial Catalog=Nhom6;Integrated Security=True");
         int views = 0;
         [Route("Story/Truyen/{StoryID}")]
         public ActionResult Truyen(int StoryID)
